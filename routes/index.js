300acd50a72dd126e1a20a4c1de744f8ff358a46
Cambri Curtis <cambric4@gmail.com>
2025-09-08 20:17:56 -0600

    Created and finish basics of most the files.

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amanda",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    }
+];
+
+router.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages});
+});
+
+router.get("/new", (req, res) => {
+    res.render("form", { title: "New Message" });
+});
+
+router.post("/new", (req, res) => {
+  const messageUser = req.body.messageUser;
+  const messageText = req.body.messageText;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+Router.get("/message/:id", (req, res) => {
+    const message = messages[req.params.id];
+    if (!message) {
+        return res.status(404).send("Message not found.");
+    }
+    res.render("message", { title: "Message Details", message});
+});
+
+module.exports = router;
